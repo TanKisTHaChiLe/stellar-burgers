a651fd060ce99ae1a87c6c14aa7361b2c7ad7dde
Artem Lanets <lanetsarn@yandex.ru>
2025-09-25 11:08:41 +0300

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './components/app/app';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
-
+// initial commit
 root.render(
   <React.StrictMode>
     <App />
